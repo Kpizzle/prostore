45f997d1252b56266cc512ec47b0c0caa958b755
Kyle Potter <kp1289@gmail.com>
2025-02-14 15:37:50 +0000

    actions

--- components/shared/header/user-button.jsx
+++ components/shared/header/user-button.jsx
@@ -24,7 +24,38 @@ const UserButton = async () => {
     );
   }
 
-  return <>User</>;
+  const firstInital = session.user?.name?.charAt(0).toLocaleUpperCase() ?? 'U';
+
+  return (
+    <div className='flex gap-2 items-center'>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className='flex items-center'>
+            <Button
+              variant='ghost'
+              className='relative w-8 h-8 rounded-full ml-2 flex items-center justify-center bg-gray-200'>
+              {firstInital}
+            </Button>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className='w-56'
+          align='end'
+          forceMount>
+          <DropdownMenuLabel className='font-normal'>
+            <div className='flex flex-col space-y-1'>
+              <div className='text-sm font-medium leading-none'>
+                {session.user?.name}
+              </div>
+              <div className='text-sm text-muted-foreground leading-none'>
+                {session.user?.email}
+              </div>
+            </div>
+          </DropdownMenuLabel>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 };
 
 export default UserButton;
